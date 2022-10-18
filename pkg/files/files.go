09f419152caf3acd0093e2fa5c7b5adce7f18714
Brendan Cazier <admin@brendancazier.com>
2022-10-17 22:59:56 -0400

    Removing unused file functions

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -1,10 +1,7 @@
 package files
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 func Exists(file string) bool {
@@ -13,17 +10,4 @@ func Exists(file string) bool {
 		return false
 	}
 	return !resp.IsDir()
-}
-
-func StripExtensions(fp string) (directory, base string) {
-	directory = filepath.Dir(fp)
-	file := filepath.Base(fp)
-
-	return directory, strings.Split(file, ".")[0]
-}
-
-func WithExtension(fp string, ext string) (output string) {
-	directory, file := StripExtensions(fp)
-
-	return filepath.Join(directory, fmt.Sprintf("%s.%s", file, ext))
 }
