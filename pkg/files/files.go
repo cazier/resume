63fd7e16c9f782192920e4d7d708aa0d38ea2c96
Brendan Cazier <admin@brendancazier.com>
2022-10-19 12:06:56 +0000

    Catching a permission error

--- pkg/files/files.go
+++ pkg/files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 
@@ -11,6 +12,8 @@ func Exists(path string, is_file bool) bool {
 	resp, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
+	} else if errors.Is(err, os.ErrPermission) {
+		shared.Exit(1, "The destination path (%s) has bad permissions", path)
 	}
 	return is_file != resp.IsDir()
 }
--- pkg/shared/version.go
+++ pkg/shared/version.go
@@ -6,7 +6,7 @@ import (
 
 const major int = 1
 const minor int = 1
-const patch int = 1
+const patch int = 2
 
 func GetVersion() string {
 	return fmt.Sprintf("v%d.%d.%d\n", major, minor, patch)
